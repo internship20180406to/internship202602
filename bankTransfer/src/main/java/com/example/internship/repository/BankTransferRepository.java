a24bf5d17013bcd79f4490fa28c00816be47e4d1
ffg-training <internship0216to0222@gmail.com>
2026-02-18 06:06:08 +0900

    合計取引金額の表示

--- bankTransfer/src/main/java/com/example/internship/repository/BankTransferRepository.java
+++ bankTransfer/src/main/java/com/example/internship/repository/BankTransferRepository.java
@@ -4,12 +4,8 @@ import com.example.internship.entity.BankTransferForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Repository
