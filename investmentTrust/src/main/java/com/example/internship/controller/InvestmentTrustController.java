da697da64c429687827f6cb2413c60dd857d385d
ffg-training <internship0216to0222@gmail.com>
2026-02-17 10:27:42 +0900

    申し込み終わり

--- investmentTrust/src/main/java/com/example/internship/controller/InvestmentTrustController.java
+++ investmentTrust/src/main/java/com/example/internship/controller/InvestmentTrustController.java
@@ -21,25 +21,35 @@ public class InvestmentTrustController {
     @GetMapping("/investmentTrust")
     public String bankTransfer(Model model) {
         List<String> bankList = new ArrayList<>();
+        List<String> subjectNameList = new ArrayList<>();
         List<String> brandNameList = new ArrayList<>();
         bankList.add("こめ銀行");
         bankList.add("ぱん銀行");
         bankList.add("麺銀行");
+        subjectNameList.add("普通預金");
+        subjectNameList.add("当座預金");
+        subjectNameList.add("貯蓄預金");
         brandNameList.add("ツナマヨ");
         brandNameList.add("梅干し");
         brandNameList.add("鮭");
         model.addAttribute("investmentTrustApplication", new InvestmentTrustForm());
         model.addAttribute("nameOptions", bankList);
         model.addAttribute("brandNameOptions", brandNameList);
+        model.addAttribute("subjectNameOptions", subjectNameList);
         return "investmentTrustMain";
     }
 
     @PostMapping("/investmentTrustConfirmation")
     public String confirmation(@ModelAttribute InvestmentTrustForm investmentTrustForm, Model model) {
-        investmentTrustForm.setBankName("ながれぼし銀行");
+
         model.addAttribute("bankName", investmentTrustForm.getBankName());
+        model.addAttribute("branchName", investmentTrustForm.getBranchName());
+        model.addAttribute("subjectName", investmentTrustForm.getSubjectName());
         model.addAttribute("bankAccountNum", investmentTrustForm.getBankAccountNum());
-        model.addAttribute("amount", investmentTrustForm.getBankAccountNum());
+        model.addAttribute("accountName", investmentTrustForm.getAccountName());
+        model.addAttribute("bankName", investmentTrustForm.getBankName());
+        model.addAttribute("amount", investmentTrustForm.getAmount());
+        model.addAttribute("brandName", investmentTrustForm.getBrandName());
         model.addAttribute("investmentTrustApplication", investmentTrustForm);
         return "investmentTrustConfirmation";
     }
--- investmentTrust/src/main/java/com/example/internship/entity/InvestmentTrustForm.java
+++ investmentTrust/src/main/java/com/example/internship/entity/InvestmentTrustForm.java
@@ -12,40 +12,62 @@ public class InvestmentTrustForm {
     @NonNull
     private String bankName;
     @NonNull
+    private String subjectName;
+    @NonNull
     private Integer bankAccountNum;
     @NonNull
     private Integer accountName;
     @NonNull
+    private String  brandName;
+    @NonNull
     private Integer amount;
+    @NonNull
+    private Integer branchName;
 
     public String getBankName() {
         return bankName;
     }
-
     public void setBankName(String bankName) {
         this.bankName = bankName;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
     }
 
     public Integer getBankAccountNum() {
         return bankAccountNum;
     }
-
-    public void setBankAccountNum(Integer bankAccountNum) {
-        this.bankAccountNum = bankAccountNum;
-    }
+    public void setBankAccountNum(Integer bankAccountNum) {this.bankAccountNum = bankAccountNum;}
 
     public Integer getAccountName() {
         return accountName;
     }
-
     public void setAccountName(Integer accountName) {
         this.accountName = accountName;
     }
+
+    public String getBrandName() {
+        return brandName;
+    }
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
     public Integer getAmount() {
         return amount;
     }
-
     public void setAmount(Integer amount) {
         this.amount = amount;
+    }
+
+    public Integer getBranchName() {
+        return branchName;
+    }
+    public void setBranchName(Integer branchName) {
+        this.branchName = branchName;
     }
 }
