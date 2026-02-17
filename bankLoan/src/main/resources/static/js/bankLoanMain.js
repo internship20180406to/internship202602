340e36bb5263b75d96c341144c0cd501482168e2
ffg-training <internship0216to0222@gmail.com>
2026-02-17 16:12:37 +0900

    確認ボタンの有効化条件変更

--- bankLoan/src/main/resources/static/js/bankLoanMain.js
+++ bankLoan/src/main/resources/static/js/bankLoanMain.js
@@ -1,3 +1,16 @@
+document.getElementById("submitButton").addEventListener("mouseover",function(){
+    if (document.getElementById("inputBankAccountNum").classList.contains("backgroundRed")===false){
+        document.getElementById("submitButton").disabled = false;
+        console.log("abled")
+        console.log(document.getElementById("submitButton").classList)
+    }else{
+        document.getElementById("submitButton").disabled = true;
+        console.log("disabled")
+    }
+});
+
+//
+
 //口座番号入力修正
     function changeNum(a){
     a.value=a.value.replace(/[^0-9０-９]/g,'').replace('０','0').replace('１','1').replace('２','2').replace('３','3').replace('４','4').replace('５','5').replace('６','6').replace('７','7').replace('８','8').replace('９','9')
@@ -15,14 +28,15 @@
         // バリデーション処理
         if (input.length === 7) {
         　　//ボタンを有効化
-            document.getElementById("submitButton").disabled = false;
+            //document.getElementById("submitButton").disabled = false;
             document.getElementById(where).classList.remove("backgroundRed")
             //console.log("abled")
             //changeAction(selectedName);
         } else {
             //ボタンを無効化
-            document.getElementById("submitButton").disabled = true;
+            //document.getElementById("submitButton").disabled = true;
             document.getElementById(where).classList.add("backgroundRed")
             //console.log("disabled")
         }
     }
+
