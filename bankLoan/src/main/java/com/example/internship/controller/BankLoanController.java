f5af701d4621041d6f2be702d6b43dae88a0e7af
ffg-training <internship0216to0222@gmail.com>
2026-02-18 15:56:42 +0900

    input type="hidden"

--- bankLoan/src/main/java/com/example/internship/controller/BankLoanController.java
+++ bankLoan/src/main/java/com/example/internship/controller/BankLoanController.java
@@ -16,19 +16,18 @@ public class BankLoanController {
     @Autowired
     private ApplyBankLoanService applyBankLoanService;
 
-    @GetMapping("/bankLoan")
+    @GetMapping("/bankLoan")//ブラウザで http://.../bankLoan にアクセスしたとき
     public String bankTransfer(Model model) {
-        model.addAttribute("bankLoanApplication", new BankLoanForm());
-        model.addAttribute("nameOptions", List.of("A銀行", "B銀行", "C銀行"));
+        model.addAttribute("bankLoanApplication", new BankLoanForm());//modelの中に、BankLoanFormの型で"bankLoanApplication"という名前の空の箱を作成する
+        model.addAttribute("nameOptions", List.of("A銀行", "B銀行", "C銀行"));//modelの中に、""nameOptions""という名前のリストを作成する
         model.addAttribute("nameOptions1", List.of("a支店", "b支店", "c支店"));
-        model.addAttribute("nameOptions2", List.of("普通預金", "当座預金","定期預金","貯蓄預金","納税準備預金","積立定期預金","通知預金","財形預金", "別段預金"));
+        model.addAttribute("nameOptions2", List.of("普通預金", "当座預金","定期預金","貯蓄預金"));
         return "bankLoanMain";
     }
 
 
     @PostMapping("/bankLoanConfirmation")
     public String confirmation(@ModelAttribute BankLoanForm bankLoanForm, Model model) {
-        //bankLoanForm.setBankName("ながれぼし銀行");
         model.addAttribute("bankName", bankLoanForm.getBankName());
         model.addAttribute("branchName", bankLoanForm.getBranchName());
         model.addAttribute("bankAccountNum", bankLoanForm.getBankAccountNum());
@@ -46,5 +45,7 @@ public class BankLoanController {
         applyBankLoanService.applyBankLoan(bankLoanForm);
         return "bankLoanCompletion";
     }
+
+
 
 }
